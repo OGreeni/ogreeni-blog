8f8ce647094de9974f74aab15b05b13161600b04
Omri Green <omri.green1@gmail.com>
2022-09-11 22:19:08 -0400

    feat: add login feature unavailable page

---
+++ pages/login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './login.module.css';
+
+const Login = () => {
+  return (
+    <div className={styles.unavailable}>
+      Whoops! This feature isn't available yet.
+    </div>
+  );
+};
+
+export default Login;
