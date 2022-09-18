04be33cffc1dad84debbe5248afa97b94ba54027
Omri Green <omri.green1@gmail.com>
2022-09-18 01:08:45 -0400

    refactor: extract contact form component into separate file

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,6 +6,9 @@ import {
 } from '../firebase/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
+import Layout from '../components/layout';
+import styles from './login.module.css';
+
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -14,44 +17,85 @@ const Login = () => {
   useEffect(() => {
     if (loading) {
       // loading screen
+      console.log('LOADING...');
       return;
     }
     if (user) {
-      console.log('LOGGED IN!');
+      console.log('USER LOGGED IN');
     }
   }, [loading]);
 
   return (
-    <>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <input
-        type="text"
-        value={email}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          setEmail(e.target.value)
-        }
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          setPassword(e.target.value)
-        }
-      />
-      <button onClick={() => logInWithEmailAndPassword(email, password)}>
-        Login
-      </button>
-      <button onClick={signInWithGoogle}>Login with Google</button>
-
-      <div>FORGOT PASSWORD</div>
-      <div>DONT HAVE AN ACCOUNT?</div>
-    </>
+    <Layout>
+      <form className={styles.contactForm}>
+        <label htmlFor="email" className={styles.inputLabel}>
+          Email:
+        </label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setEmail(e.target.value)
+          }
+        />
+        <span className={styles.inputUnderline}></span>
+        <label htmlFor="password" className={styles.inputLabel}>
+          Password:
+        </label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setPassword(e.target.value)
+          }
+        />
+        <span className={styles.inputUnderline}></span>
+        <button onClick={() => logInWithEmailAndPassword(email, password)}>
+          Login
+        </button>
+        <button onClick={signInWithGoogle}>Login with Google</button>
+        <div>FORGOT PASSWORD</div>
+        <div>Don't have an account? Register</div>
+      </form>
+    </Layout>
   );
 };
+
+{
+  /* <form className={styles.contactForm} onSubmit={formOnSubmit}>
+<h3>Thank you for your interest.</h3>
+<h4>I'll reach out to you as soon as I can!</h4>
+<label htmlFor="subject" className={styles.inputLabel}>
+  Subject:
+</label>
+<input
+  type="text"
+  name="subject"
+  id="subject"
+  required
+  value={subjectValue}
+  onChange={(e) => setSubjectValue(e.target.value)}
+/>
+<span className={styles.inputUnderline}></span>
+<label htmlFor="email" className={styles.inputLabel}>
+  Email:
+</label>
+<input
+  type="email"
+  name="email"
+  id="email"
+  required
+  value={emailValue}
+  onChange={(e) => setEmailValue(e.target.value)}
+/>
+<span className={styles.inputUnderline}></span>
+<Button onClick={() => console.log('Clicked')}>Submit</Button>
+</form>
+{formSubmitted && (
+<div className={styles.success}>Form submitted successfully!</div>
+)} */
+}
 
 export default Login;
